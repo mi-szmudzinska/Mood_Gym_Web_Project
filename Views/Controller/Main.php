d25e1d9fee2ecaf8b4be90a42e4cf7b2f7855b68
mi-szmudzinska <miszmudzinska@gmail.com>
2019-12-09 22:56:55 +0100

    Fix Navbar & css style

--- Views/Controller/Main.php
+++ Views/Controller/Main.php
@@ -1,10 +1,26 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Dokument bez tytułu</title>
-</head>
-
 <body>
+<?php include(dirname(__DIR__).'/navbar.html') ?>
+	
+<div class="container-md ">
+  	<div class="logo2"> 
+		<img src="../Public/img/logo2.png">
+	</div>
+	<h5 class="text-muted" >Zaloguj się</h5>
+  	<p class="text-muted">Zaloguj się za pomocą e-mail:</p>
+	<div align="center">
+	<form>
+        <input name="email" type="text" placeholder="email@email.com">
+        <input name="password" type="password" placeholder="password">
+			<p class="text-muted indeks">Nie pamiętasz hasła?</p>
+		
+		<button type="submit">Zaloguj</button>
+		<button type="submit2">Zarejestruj się</button>
+    </form>
+
+	</div>
+</div>
+
 </body>
 </html>
